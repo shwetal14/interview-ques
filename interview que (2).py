f0491d8f727650db302f08f95f16dfd28ed49ba1
Shwetal Arun Chachane <95999701+shwetal14@users.noreply.github.com>
2022-02-15 16:42:08 +0530

    Add files via upload

---
+++ interview que (2).py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+# 20 interview questions for Data Types, Operators, Conditional Statements, Looping Statements, Functions.
+
+
+
+
+1) What is the difference between lists and tuples
+
+2) What is __init__ in Python
+
+3) What are local variables and global variables in Python
+
+4) What does [::-1] do
+
+5) Do we need to declare variables with data types in Python
+
+6) What is slicing in Python
+
+7) What does len() do
+
+8) Why do we need break and continue in Python
+
+9) What do you know about relational operators in Python
+
+10) What is the difference between .py and .pyc files
+
+11) What does this mean: *args, **kwargs? And why would we use it
+    
+12) What does elif mean
+
+13) Using a dictionary, create an equivalent to a switch case statement
+
+14) Which conditional statements are available in Python
+
+15) Which looping statements are available in Python 
+    
+16) How do you declare a variable in Python  
+
+17) How to you initialize an array 
+
+18) What is Frozen Set in Python 
+
+19) Name four of the main data types in Python
+
+20) Differentiate between mutable and immutable objects
+
